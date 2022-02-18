a2a0014f75f34ff8a3bf293d6de414e6224d83dd
hyeon <liebe244@naver.com>
2022-02-18 22:13:22 +0900

    update code

--- index.js
+++ index.js
@@ -5,46 +5,21 @@ const errorText = document.querySelector(".errorText");
 const firstTable = document.querySelectorAll(".first");
 
 let count = 0;
+const valueArray = [];
 
-const handleReset = () => {
-  count = 0;
-  gameResult.innerText = "";
-  errorText.innerText = "";
-  firstTable.forEach((text) => {
-    text.innerText = "";
+const inputValue = () => {
+  input.forEach((text) => {
+    valueArray.push(text.value);
   });
-};
-
-const tableValue = () => {
-  if (count < 9) {
-    input.forEach((text) => {
-      if (text.value == "") {
-        errorText.innerText = "빈 칸 없이 숫자를 입력해주세요.";
-        firstTable[count].innerText = "";
-        errorCheck = true;
-      } else {
-        errorText.innerText = "";
-        firstTable[count].innerText += text.value;
-        text.value = "";
-        errorCheck = false;
-      }
-    });
-  } else {
-    gameResult.innerText = "정답은 ----입니다.";
-  }
-  if (errorCheck == false) {
-    count += 1;
-  }
 };
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     switch (button.dataset.value) {
       case "result":
-        tableValue();
+        inputValue();
         break;
       case "reset":
-        handleReset();
         break;
     }
   });
