640bd5371eb758ae7b8573704dc30141d8f2c136
hyeon <liebe244@naver.com>
2022-02-20 20:13:49 +0900

    enter array values

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const errorText = document.querySelector(".errorText");
 const firstTable = document.querySelectorAll(".first");
 
 let count = 0;
-const valueArray = [];
 
 const inputValue = () => {
+  const valueArray = [];
   input.forEach((text) => {
     valueArray.push(text.value);
   });
+  firstTable[count].innerText = valueArray;
+  count += 1;
 };
 
 buttons.forEach((button) => {
