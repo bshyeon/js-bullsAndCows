e1f0cf8f08262925f2d487b8b9985b73859f7ff7
hyeon <liebe244@naver.com>
2022-02-22 21:24:05 +0900

    ball and strike

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const buttons = document.querySelectorAll("button");
 const gameResult = document.querySelector(".gameResult");
 const errorText = document.querySelector(".errorText");
 const firstTable = document.querySelectorAll(".first");
+const secondTable = document.querySelectorAll(".second");
 
 let count = 0;
-let randomArray = [];
+let randomArray = new Array();
+let valueArray = new Array();
 
 const randomValue = () => {
-  const randomNumber = Math.floor(Math.random() * 10);
-  if (randomArray.length < 4 && randomArray.indexOf(randomNumber) < 0) {
-    randomArray.push(randomNumber);
+  while (randomArray.length < 4) {
+    const randomNumber = Math.floor(Math.random() * 10);
+    if (randomArray.indexOf(randomNumber) < 0) {
+      randomArray.push(randomNumber);
+    }
   }
 };
 
 const inputValue = () => {
-  const valueArray = [];
+  valueArray = [];
   input.forEach((text) => {
     valueArray.push(parseInt(text.value));
   });
@@ -36,6 +40,7 @@ const inputValue = () => {
   } else {
     errorText.innerText = "";
     firstTable[count].innerText = valueArray;
+    ballAndStrike();
     count += 1;
   }
 };
@@ -46,10 +51,31 @@ const reset = () => {
   firstTable.forEach((item) => {
     item.innerText = "";
   });
+  secondTable.forEach((item) => {
+    item.innerText = "";
+  });
   for (let i = 0; i < input.length; i++) {
     input[i].value = "";
   }
   count = 0;
+};
+
+const ballAndStrike = () => {
+  console.log(valueArray);
+  console.log(randomArray);
+  let ball = 0;
+  let strike = 0;
+  for (let i = 0; i < valueArray.length; i++) {
+    if (valueArray[i] === randomArray[i]) {
+      strike += 1;
+    } else if (
+      valueArray[i] !== randomArray[i] &&
+      randomArray.indexOf(valueArray[i]) >= 0
+    ) {
+      ball += 1;
+    }
+  }
+  secondTable[count].innerText = `${strike}S ${ball}B`;
 };
 
 randomValue();
