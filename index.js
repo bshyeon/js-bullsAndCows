d225ae3d1080a0cbbb2f516b098a8adbfda2f5c5
hyeon <liebe244@naver.com>
2022-02-22 17:58:20 +0900

    reset function

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const errorText = document.querySelector(".errorText");
 const firstTable = document.querySelectorAll(".first");
 
 let count = 0;
-
 const inputValue = () => {
   const valueArray = [];
   input.forEach((text) => {
@@ -17,19 +16,30 @@ const inputValue = () => {
   const valueRangeCheck = valueArray.find((item) => {
     return item >= 10;
   });
-  const set = new Set(valueArray);
+  const duplicateNumber = new Set(valueArray);
 
   if (blankCheck || nanCheck) {
     errorText.innerText = "숫자를 입력해주세요.";
   } else if (valueRangeCheck) {
     errorText.innerText = "10미만의 숫자를 입력해주세요.";
-  } else if (set.size < 4) {
+  } else if (duplicateNumber.size < 4) {
     errorText.innerText = "중복된 숫자가 있습니다.";
   } else {
     errorText.innerText = "";
     firstTable[count].innerText = valueArray;
     count += 1;
   }
+};
+
+const reset = () => {
+  errorText.innerText = "";
+  firstTable.forEach((item) => {
+    item.innerText = "";
+  });
+  for (let i = 0; i < input.length; i++) {
+    input[i].value = "";
+  }
+  count = 0;
 };
 
 buttons.forEach((button) => {
@@ -39,7 +49,7 @@ buttons.forEach((button) => {
         inputValue();
         break;
       case "reset":
-        // reset 함수 추가
+        reset();
         break;
     }
   });
